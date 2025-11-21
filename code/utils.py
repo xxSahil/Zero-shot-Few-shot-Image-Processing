2b5f874784c99c88cd1596134dcc32b10518f116
xxSahil <sahilsminhas@gmail.com>
2025-11-20 19:00:22 -0500

    code

---
+++ code/utils.py
@@ -0,0 +1,11 @@
+"""
+Contains helper functions for zero-shot and few-shot
+
+1. Loading an image with with OpenCV
+2. Converting BGR to RGB for CLIP and Matplot
+3. Converting an RGB NumPy array to PIL for CLIP
+4. File name
+5. Creating a Matplot diagram for zero-shot and few-shot
+
+
+"""
